74f8cbbc6cf7578559ffdf561de802ea546364e7
ABonnafous <abonnafous@netizis.com>
2022-10-05 18:03:17 +0200

    Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [
+    vue(),
+    storyblok({
+      accessToken: "TKO7rbmQguIvlGTUCclpiwtt",
+      components: {
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        page: "storyblok/Page",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
---
+++ src/components/Code.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  code: object | null;
+}>();
+</script>
+
+<template>
+  <pre>
+    {{ props.code }}
+  </pre>
+</template>
